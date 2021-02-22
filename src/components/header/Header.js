d2c0f206e85c349c4e458372d5ef264a0a86b206
Husdady <husdadymena@gmail.com>
2021-02-21 19:21:52 -0500

    finish

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 import './css/header.css';
-import Icon from '../assets/Icon';
 
 const Header = props => {
   const { themeColor, setThemeColor } = props;
@@ -8,10 +7,7 @@ const Header = props => {
       <div className="logo">
         <img src={`${process.env.PUBLIC_URL}/img/icon.png`} alt="icon" />&nbsp;&nbsp;<span>SC</span><span>amfie<sup>*</sup></span>
       </div>
-      <button onClick={() => setThemeColor(!themeColor)}>
-        <Icon className="fas fa-sun" />
-        <Icon className="fas fa-moon-stars" />
-      </button>
+      <button onClick={() => setThemeColor(!themeColor)}></button>
     </header>
   )
 }
